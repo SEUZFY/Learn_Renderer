f6ac2c65e61e894d86081d25caa87b943ba5852f
zmocheng <zmocheng@gmail.com>
2022-05-10 23:39:17 +0200

    Optimize the code according to: https://github.com/ssloy/tinyrenderer/issues/28.

--- src/main.cpp
+++ src/main.cpp
@@ -15,23 +15,32 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
         std::swap(x0, x1);
         std::swap(y0, y1);
     }
+
     int dx = x1 - x0;
     int dy = y1 - y0;
     int derror2 = std::abs(dy) * 2;
     int error2 = 0;
     int y = y0;
-    for (int x = x0; x <= x1; x++) {
-        if (steep) {
-            image.set(y, x, TGAColor(255, 1));
-        }
-        else {
-            image.set(x, y, TGAColor(255, 1));
-        }
-        error2 += derror2;
+    int y_increment = y1 > y0 ? 1 : -1;
 
-        if (error2 > dx) {
-            y += (y1 > y0 ? 1 : -1);
-            error2 -= dx * 2;
+    if (steep) {
+        for (int x = x0; x <= x1; ++x) {
+            image.set(y, x, TGAColor(255, 1));
+            error2 += derror2;
+            if (error2 > dx) {
+                y += y_increment;
+                error2 -= dx * 2;
+            }
+        }
+    }
+    else {
+        for (int x = x0; x <= x1; ++x) {
+            image.set(x, y, TGAColor(255, 1));
+            error2 += derror2;
+            if (error2 > dx) {
+                y += y_increment;
+                error2 -= dx * 2;
+            }
         }
     }
 }
